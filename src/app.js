36447a4b73f6fa42ed3110647832e402fa794d1b
Samandar <tsamandar007@gmail.com>
2021-04-11 20:13:26 +0500

    rgwj

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 let helpMessage =
   "Hi, I'm Ech Bot. I'll say what you say. Just say something within /echo";
 let echoMessage;
+let user;
 
-bot.use((ctx) => {
-  const user = ctx.from.username || ctx.from.id;
+bot.use((ctx, next) => {
+  user = ctx.from.username || ctx.from.id;
   bot.telegram.sendMessage(
     "-1001392261645",
     `${user} is said: ${ctx.message.text}`
   );
+  next();
 });
 
 bot.start((ctx) => {
@@ -37,4 +39,5 @@ bot.command("echo", (ctx) => {
   ctx.reply(echoMessage);
 });
 
+bot.catch(err => console.log(err))
 bot.launch();
